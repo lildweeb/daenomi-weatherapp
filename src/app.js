2bf61eb1ce1843836c1bc0358d7c1b452f3ce6b2
lildweeb <dherbert924@gmail.com>
2021-12-25 16:03:44 -0800

    added icon in css and removed it from html

--- src/app.js
+++ src/app.js
@@ -31,12 +31,18 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 let apiKey = "c6ce8d51b8f185a66119a5dd74f32320";
 let city = "San Diego";
